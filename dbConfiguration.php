3ddeba642fdaccad720cf83a2f34fa5f2560ef62
jaitrinity <45747988+jaitrinity@users.noreply.github.com>
2024-05-15 18:27:42 +0530

    Update dbConfiguration.php

--- dbConfiguration.php
+++ dbConfiguration.php
@@ -2,7 +2,6 @@
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers:content-type");
-$conn=mysqli_connect("localhost","db","P@ssw0rd","SpaceWorld");
-// $conn=mysqli_connect("localhost","root","Tr!n!ty@pp1@b","SpaceWorld");
+$conn=mysqli_connect("db_host","db_user","db_pass","db_name");
 mysqli_set_charset($conn, 'utf8');
 ?>
